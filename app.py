4e4daadc42914b2c25a88e5402f559192464ca7f
Samarth Hattangady <samhattangady@gmail.com>
2019-08-30 01:06:43 +0530

    fix: cherrypy config for heroku

--- app.py
+++ app.py
@@ -35,4 +35,7 @@ class StockHound:
 
 
 if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+    cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
     cherrypy.quickstart(StockHound())
+
